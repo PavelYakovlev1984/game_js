a9efd7a26f1c8762862c1f1c08c58bea4276150f
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-06 22:35:51 +0300

    Update _components.js

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,12 @@
+"use strict";
 // console.log('components');
-document.querySelector('.check').addEventListener('click', )
+// document.querySelector('.check').addEventListener('click', function() {
+//   console.log(document.querySelector('.number-input').value);
+// })
+document.querySelector('.check').addEventListener('click', function(){
+  // console.log(document.querySelector('.number-input').value);
+  // document.querySelector('.guess-message').textContent = 'Правильно!';
+  let guessingNumber = Number(document.querySelector('.number-input').value);
+  console.log(guessingNumber, typeof guessingNumber);
+})
 
