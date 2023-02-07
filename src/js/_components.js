b3bfdfdfcd0d315145b4e5933b719bfe4b07d517
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-07 22:17:28 +0300

    Update _components.js

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,40 +3,63 @@
 // document.querySelector('.check').addEventListener('click', function() {
 //   console.log(document.querySelector('.number-input').value);
 // })
-let score = 20;
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+// let score = 20;
+// let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// console.log(secretNumber);
 
-document.querySelector('.question').textContent = secretNumber;
+// document.querySelector('.question').textContent = secretNumber;
 
-document.querySelector('.check').addEventListener('click', function(){
+// document.querySelector('.check').addEventListener('click', function(){
   // console.log(document.querySelector('.number-input').value);
   // document.querySelector('.guess-message').textContent = 'Правильно!';
-  let guessingNumber = Number(document.querySelector('.number-input').value);
-  console.log(guessingNumber, typeof guessingNumber);
+//   let guessingNumber = Number(document.querySelector('.number-input').value);
+//   console.log(guessingNumber, typeof guessingNumber);
 
-  if(!guessingNumber){
+//   if(!guessingNumber){
+//     document.querySelector('.guess-message').textContent = 'Введите число!';
+//   } else if(secretNumber === guessingNumber){
+//     document.querySelector('.guess-message').textContent = 'Правильно!';
+//   } else if(guessingNumber > secretNumber){
+//     if(score>1){
+//     document.querySelector('.guess-message').textContent = 'Слишком много!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.guess-message').textContent = 'Game over!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   } else if(guessingNumber < secretNumber){
+//     if(score>1){
+//     document.querySelector('.guess-message').textContent = 'Слишком мало!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.guess-message').textContent = 'Game over!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// })
+
+
+
+// the second game
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.question').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function (){
+  let guessingNumber = document.querySelector('.number-input').value
+  console.log(guessingNumber);
+
+  if(!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
-  } else if(secretNumber === guessingNumber){
-    document.querySelector('.guess-message').textContent = 'Правильно!';
-  } else if(guessingNumber > secretNumber){
-    if(score>1){
+  } else if(guessingNumber > secretNumber) {
     document.querySelector('.guess-message').textContent = 'Слишком много!';
     score--;
     document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.guess-message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = 0;
-    }
   } else if(guessingNumber < secretNumber){
-    if(score>1){
     document.querySelector('.guess-message').textContent = 'Слишком мало!';
     score--;
     document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.guess-message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = 0;
-    }
   }
 })
-
