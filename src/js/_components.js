894a300a9cc2e43098a4a241d4a6c303926a418a
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-09 22:32:46 +0300

    Update _components.js

--- src/js/_components.js
+++ src/js/_components.js
@@ -99,11 +99,10 @@ document.querySelector('.check').addEventListener('click', function (){
   } else if(guessingNumber === secretNumber) {
     document.querySelector('.guess-message').textContent = 'Выиграл!';
     document.querySelector('.page__body').style.backgroundColor = '#987aeb';
-    // score = highScore;
-    highScore = Number(document.querySelector('.highscore').textContent);
     if(score > highScore){
-      score = highScore;
-  }
+      highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+    }
   } else if(guessingNumber > secretNumber) {
     if(score > 0) {
     document.querySelector('.guess-message').textContent = 'Слишком много!';
@@ -128,5 +127,6 @@ document.querySelector('.again').addEventListener('click', function(){
   document.querySelector('.score').textContent = score;
   document.querySelector('.page__body').style.backgroundColor = '#000';
   document.querySelector('.guess-message').textContent = 'Начни угадывать!';
-  document.querySelector('.number-input').textContent = '';
+  document.querySelector('.number-input').value = '';secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.question').textContent = secretNumber;
 })
